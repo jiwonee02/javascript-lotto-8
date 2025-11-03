e32964fce8c84be2344b51faeb1fd23afecf5a1b
jiwonee02 <nepthun@naver.com>
2025-11-03 15:52:40 +0900

    feat: 상수 메시지 분리

---
+++ src/core/constants.js
@@ -0,0 +1,19 @@
+export const PRICE = 1000;
+export const MIN = 1;
+export const MAX = 45;
+export const PICK_COUNT = 6;
+
+export const PRIZE = {
+    1: 2000000000, // 1등
+    2: 30000000, // 2등
+    3: 1500000, // 3등
+    4: 50000, // 4등
+    5: 5000, // 5등
+};
+
+export const OUTPUT = {
+    STAT_HEADER: "당첨 통계",
+    STAT_RULE: "---",
+    RATE_PREFIX: "총 수익률은 ",
+    RATE_SUFFIX: "%입니다.",
+};
