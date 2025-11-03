9d62753d5198166e15ed5a3ad4c01d3b8570a55f
jiwonee02 <nepthun@naver.com>
2025-11-03 17:21:15 +0900

    feat: 앱 흐름 구성 및 재입력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { InputView } from "./io/InputView.js";
+import { OutputView } from "./io/OutputView.js";
+import { LottoService } from "./service/LottoService.js";
+
 class App {
-  async run() {}
+    #svc = new LottoService();
+
+    async run() {
+        try {
+            const money = await this.#retry(InputView.readMoney);
+            const tickets = this.#svc.issueTickets(money);
+            OutputView.printIssued(tickets);
+
+            const win = await this.#retry(InputView.readWinNumbers);
+            const bonus = await this.#retry(() =>
+                InputView.readBonusNumber(win)
+            );
+
+            const counts = this.#svc.aggregate(tickets, win, bonus);
+            const prize = this.#svc.totalPrize(counts);
+            const rate = this.#svc.rate(prize, money);
+            OutputView.printStats(counts, rate);
+        } catch {}
+    }
+
+    async #retry(fn) {
+        for (;;) {
+            try {
+                return await fn();
+            } catch (e) {
+                OutputView.printError(e);
+            }
+        }
+    }
 }
 
 export default App;
