ec6f596fcf886d410627b86965bdb3052c68f32b
jiwonee02 <nepthun@naver.com>
2025-11-03 15:57:15 +0900

    feat: 에러  메시지 분리

---
+++ src/core/errors.js
@@ -0,0 +1,12 @@
+export const ERROR = {
+    MONEY_FORMAT: "[ERROR] 구입 금액 형식이 올바르지 않습니다.",
+    MONEY_UNIT: "[ERROR] 구입 금액은 1,000원 단위여야 합니다.",
+    WIN_NUM_COUNT: "[ERROR] 당첨 번호는 6개여야 합니다.",
+    WIN_NUM_FORMAT: "[ERROR] 당첨 번호 형식이 올바르지 않습니다.",
+    WIN_NUM_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+    WIN_NUM_DUP: "[ERROR] 당첨 번호에 중복이 있습니다.",
+    BONUS_FORMAT: "[ERROR] 보너스 번호가 올바르지 않습니다.",
+    BONUS_RANGE: "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.",
+    BONUS_DUP: "[ERROR] 보너스 번호가 당첨 번호와 중복입니다.",
+    OUTPUT_ERROR: "[ERROR] 출력 중 문제가 발생했습니다.",
+};
